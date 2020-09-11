82f1a57833a0365225b5906a18386b1ea9a15967
Acaruso <anthonycaruso102@gmail.com>
2020-09-10 20:04:10 -0400

    add files

---
+++ src/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+
+let win = null;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 1400,
+    height: 1000,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  
+  console.log('dir')
+  console.log(__dirname)
+  win.loadFile(__dirname + '/index.html');
+
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+})
